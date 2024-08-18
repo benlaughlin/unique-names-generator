49c351b60799fa524e0c08a4506266a8b78f468a
Ben Laughlin <ben@laughlin.com.au>
2024-08-18 17:40:11 +1000

    town varients dictionary

--- src/dictionaries/index.ts
+++ src/dictionaries/index.ts
@@ -4,7 +4,9 @@ import colors from './colors';
 import countries from './countries';
 import languages from './languages';
 import names from './names';
-import starWars from './star-wars';
 import { NumberDictionary } from './numbers';
+import starWars from './star-wars';
+import townVarients from './town-varients';
 
-export { adjectives, animals, colors, countries, languages, names, starWars, NumberDictionary };
+export { adjectives, animals, colors, countries, languages, names, NumberDictionary, starWars, townVarients };
+
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
+export {
+	NumberDictionary, adjectives,
+	animals,
+	colors,
+	countries,
+	languages,
+	names,
+	starWars,
+	townVarients
+} from './dictionaries/index';
 export { uniqueNamesGenerator } from './unique-names-generator';
 export { Config } from './unique-names-generator.constructor';
-export {
-  adjectives,
-  animals,
-  colors,
-  countries,
-  languages,
-  names,
-  starWars,
-  NumberDictionary,
-} from './dictionaries/index';
+
